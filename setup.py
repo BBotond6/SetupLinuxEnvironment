f1cf56dadef51a2e0feb20d6b3aef1a975ae8ae4
BBotond6 <bottondbencsics@gmail.com>
2023-10-19 00:08:10 +0200

    On branch master: apt updrage and bash-completion
    Implement sudo apt upgrade step in setup.sh file.
    Implement InstallBashCompletion function in setup.py
    to install bash-completion package.
    
    Changes to be committed:
    	modified:   setup.py
    	modified:   setup.sh

--- setup.py
+++ setup.py
@@ -101,44 +101,44 @@ def AddSudoPrivilegesToUser(user):
 def SetupOpenSshServer():
 	#Install openssh-server
 	if RunShCommand("sudo apt install -y openssh-server"):
-		DONE_event(4, "Install openssh-server")
+		DONE_event(5, "Install openssh-server")
 	else:
-		FAILED_event(4, "Install openssh-server", "Installation of openssh-server was unsuccessfull!")
+		FAILED_event(5, "Install openssh-server", "Installation of openssh-server was unsuccessfull!")
 	
 	#Setup openssh-server
 	CommandResult = ReadShCommandOut("sudo systemctl status ssh")
 	if CommandResult and "Active: active (running)" in CommandResult:
 		print("SSH server is active (running)")
 	else:
-		FAILED_event(5, "Setup openssh-server", "SSH server is not running!")
+		FAILED_event(6, "Setup openssh-server", "SSH server is not running!")
 
 	if RunShCommand("ufw --version"):
 		print("ufw is installed!")
 		if RunShCommand("sudo ufw allow ssh"):
 			print("Allow SSH through UFW was successfull")
 		else:
-			FAILED_event(5, "Setup openssh-server", "Allow SSH through UFW was unsuccessfull!")
+			FAILED_event(6, "Setup openssh-server", "Allow SSH through UFW was unsuccessfull!")
 	else:
 		print("ufw is not installed!")
-	DONE_event(5, "Setup openssh-server")
+	DONE_event(6, "Setup openssh-server")
 
 def SetupRemoteDesktopAccess():
 	if RunShCommand("sudo apt install xrdp"):
 		print("Install xrdp was successfull")
 	else:
-		FAILED_event(6, "Setup remote desktop access", "Install xrdp was unsuccessfull!")
+		FAILED_event(7, "Setup remote desktop access", "Install xrdp was unsuccessfull!")
 
 	if RunShCommand("sudo systemctl enable --now xrdp"):
 		print("Enable and start xrdp service was successfull")
 	else:
-		FAILED_event(6, "Setup remote desktop access", "Enable and start xrdp service was unsuccessfull!")
+		FAILED_event(7, "Setup remote desktop access", "Enable and start xrdp service was unsuccessfull!")
 
 	if RunShCommand("ufw --version"):
 		print("ufw is installed!")
 		if RunShCommand("sudo ufw allow from any to any port 3389 proto tcp"):
 			print("Allow TCP traffic on port 3389 was successfull")
 		else:
-			FAILED_event(6, "Setup remote desktop access", "Allow TCP traffic on port 3389 was unsuccessfull!")
+			FAILED_event(7, "Setup remote desktop access", "Allow TCP traffic on port 3389 was unsuccessfull!")
 	else:
 		print("ufw is not installed!")
 
@@ -148,27 +148,27 @@ def SetupRemoteDesktopAccess():
 	if RunInteractiveShCommand("sudo adduser " + NewUserName):
 		print("Create " + NewUserName + " user was successfull")
 	else:
-		FAILED_event(6, "Setup remote desktop access", "Create " + NewUserName + " user was unsuccessfull!")
+		FAILED_event(7, "Setup remote desktop access", "Create " + NewUserName + " user was unsuccessfull!")
 
 	if CreateLinuxGroup("tsusers"):
 		print("Create tsusers group was successfull")
 	else:
-		FAILED_event(6, "Setup remote desktop access", "Create tsusers group was unsuccessfull!")
+		FAILED_event(7, "Setup remote desktop access", "Create tsusers group was unsuccessfull!")
 
 	if CreateLinuxGroup("tsadmins"):
 		print("Create tsadmins group was successfull")
 	else:
-		FAILED_event(6, "Setup remote desktop access", "Create tsadmins group was unsuccessfull!")
+		FAILED_event(7, "Setup remote desktop access", "Create tsadmins group was unsuccessfull!")
 
 	if RunShCommand("sudo usermod -a -G tsusers " + NewUserName):
 		print("Add " + NewUserName + " to tsusers was successfull")
 	else:
-		FAILED_event(6, "Setup remote desktop access", "Add " + NewUserName + " to tsusers was unsuccessfull!")
+		FAILED_event(7, "Setup remote desktop access", "Add " + NewUserName + " to tsusers was unsuccessfull!")
 
 	if RunShCommand("sudo service xrdp restart"):
 		print("Restart xrdp service was successfull")
 	else:
-		FAILED_event(6, "Setup remote desktop access", "Restart xrdp service was unsuccessfull!")
+		FAILED_event(7, "Setup remote desktop access", "Restart xrdp service was unsuccessfull!")
 
 	AddSudoPrivileges = input("Do you want to give sudo privileges to " + NewUserName + " user? (y=yes, n=no): ")
 	while AddSudoPrivileges not in ("y", "n"):
@@ -177,36 +177,43 @@ def SetupRemoteDesktopAccess():
 		if AddSudoPrivilegesToUser(NewUserName):
 			print("Add sudo privileges to " + NewUserName + " user was successfull!")
 		else:
-			FAILED_event(6, "Setup remote desktop access", "Add sudo privileges to " + NewUserName + " user was unsuccessfull!")
-	DONE_event(6, "Setup remote desktop access")
+			FAILED_event(7, "Setup remote desktop access", "Add sudo privileges to " + NewUserName + " user was unsuccessfull!")
+	DONE_event(7, "Setup remote desktop access")
 
 def SetupSQLite():
 	if RunShCommand("sudo apt install sqlite3"):
-		DONE_event(7, "Setup SQLite database", "Install SQLite was successfull")
+		DONE_event(8, "Setup SQLite database", "Install SQLite was successfull")
 	else:
-		FAILED_event(7, "Setup SQLite database", "Install SQLite was unsuccessfull!")
+		FAILED_event(8, "Setup SQLite database", "Install SQLite was unsuccessfull!")
 
 def SetupCppEnvironment():
 	if RunShCommand("sudo apt install build-essential"):
-		DONE_event(8, "Install build-essential", "Install build-essential was successfull")
+		DONE_event(9, "Install build-essential", "Install build-essential was successfull")
 	else:
-		FAILED_event(8, "Install build-essential", "Install build-essential was unsuccessfull!")
+		FAILED_event(9, "Install build-essential", "Install build-essential was unsuccessfull!")
 
 	if RunShCommand("sudo apt install libpoppler-glib-dev"):
-		DONE_event(9, "Install Poppler", "Install Poppler was successfull")
+		DONE_event(10, "Install Poppler", "Install Poppler was successfull")
 	else:
-		FAILED_event(9, "Install Poppler", "Install Poppler was unsuccessfull!")
+		FAILED_event(10, "Install Poppler", "Install Poppler was unsuccessfull!")
 
-	if RunShCommand("sudo apt-get install libglib2.0-dev"):
-		DONE_event(10, "Install GLib", "Install GLib was successfull")
+	if RunShCommand("sudo apt install libglib2.0-dev"):
+		DONE_event(11, "Install GLib", "Install GLib was successfull")
 	else:
-		FAILED_event(10, "Install GLib", "Install GLib was unsuccessfull!")
+		FAILED_event(11, "Install GLib", "Install GLib was unsuccessfull!")
+
+def InstallBashCompletion():
+	if RunShCommand("sudo apt install bash-completion"):
+		DONE_event(12, "Install bash completion", "Install bash completion was successfull")
+	else:
+		FAILED_event(12, "Install bash completion", "Install bash completion was unsuccessfull!")
 
 def main():
 	SetupOpenSshServer()
 	SetupRemoteDesktopAccess()
 	SetupSQLite()
 	SetupCppEnvironment()
+	InstallBashCompletion()
 
 if __name__ == "__main__":
 	main()
--- setup.sh
+++ setup.sh
@@ -11,28 +11,31 @@ setup_py='setup.py'
 if command -v apt; then
 	echo -e "${YELLOW}Step 1:${END_COLOR} apt update \nRefresh the local package database: sudo apt get update"
 	sudo apt update
-	
 	echo -e "${YELLOW}Step 1:${END_COLOR} apt update | ${GREEN}DONE${END_COLOR}"
-	
+
+	echo -e "${YELLOW}Step 2:${END_COLOR} apt upgrade \nUpgrade installed packages: sudo apt get ugrade"
+	sudo apt upgrade
+	echo -e "${YELLOW}Step 2:${END_COLOR} apt upgrade | ${GREEN}DONE${END_COLOR}"
+
 	#Check that python3 is installed
 	if command -v python3; then
-		echo -e "${YELLOW}Step 2:${END_COLOR} Install Python3 | ${GREEN}DONE${END_COLOR} \n note: Python3 is already installed!"
+		echo -e "${YELLOW}Step 3:${END_COLOR} Install Python3 | ${GREEN}DONE${END_COLOR} \n note: Python3 is already installed!"
 	else
-		echo -e "${YELLOW}Step 2:${END_COLOR} Install Python3 \nPython3 is not installed! Installing..."
+		echo -e "${YELLOW}Step 3:${END_COLOR} Install Python3 \nPython3 is not installed! Installing..."
 		sudo apt install python3
 		if command -v python3; then
-			echo -e "${YELLOW}Step 2:${END_COLOR} Install Python3 | ${GREEN}DONE${END_COLOR}"
+			echo -e "${YELLOW}Step 3:${END_COLOR} Install Python3 | ${GREEN}DONE${END_COLOR}"
 		else
-			echo -e "${YELLOW}Step 2:${END_COLOR} Install Python3 | ${RED}FAILED${END_COLOR} \n note: Installation of Python3 was unsuccessful!"
+			echo -e "${YELLOW}Step 3:${END_COLOR} Install Python3 | ${RED}FAILED${END_COLOR} \n note: Installation of Python3 was unsuccessful!"
 			
 		fi
 	fi
 	
 	if [ -e "$setup_py" ]; then
-		echo -e "${YELLOW}Step 3:${END_COLOR} Find and run $setup_py | ${GREEN}DONE${END_COLOR} \n\nRun $setup_py for further configurations...\n"
+		echo -e "${YELLOW}Step 4:${END_COLOR} Find and run $setup_py | ${GREEN}DONE${END_COLOR} \n\nRun $setup_py for further configurations...\n"
 		python3 setup.py
 	else
-		echo -e "${YELLOW}Step 3:${END_COLOR} Find and run $setup_py | ${RED}FAILED${END_COLOR} \n note: $setup_py does not exist!"
+		echo -e "${YELLOW}Step 4:${END_COLOR} Find and run $setup_py | ${RED}FAILED${END_COLOR} \n note: $setup_py does not exist!"
 		exit 0
 	fi
 	
